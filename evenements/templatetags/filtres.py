14703dc8ed0cac08722cc322c13428de2427e20f
rtrjl <rt@zone42.fr>
2012-09-05 17:29:15 +0200

    application thème bootstrap pour equipement-details, ajout du png Y, ajout de jquery , map OSM fluide , crop automatique des images equipements , evènements

--- evenements/templatetags/filtres.py
+++ evenements/templatetags/filtres.py
@@ -73,8 +73,45 @@ def dateSEO(dateUTC):
     date = dateUTC.astimezone(TZone)
     return date.strftime("%Y")+"-"+date.strftime("%m")+"-"+date.strftime("%d")+"T"+date.strftime("%H")+":"+date.strftime("%M")
 
+# trouvé sur http://united-coders.com/christian-harms/image-resizing-tips-every-coder-should-know/
+def resizeandcrop(img, box, fit):
+    '''Downsample the image.
+    @param img: Image -  an Image-object
+    @param box: tuple(x, y) - the bounding box of the result image
+    @param fix: boolean - crop the image to fill the box
+     apuça @param out: file-like-object - save the image into the output stream
+    '''
+    #preresize image with factor 2, 4, 8 and fast algorithm
+    factor = 1
+    while img.size[0]/factor > 2*box[0] and img.size[1]*2/factor > 2*box[1]:
+        factor *=2
+    if factor > 1:
+        img.thumbnail((img.size[0]/factor, img.size[1]/factor), Image.NEAREST)
+
+    #calculate the cropping box and get the cropped part
+    if fit:
+        x1 = y1 = 0
+        x2, y2 = img.size
+        wRatio = 1.0 * x2/box[0]
+        hRatio = 1.0 * y2/box[1]
+        if hRatio > wRatio:
+            y1 = int(y2/2-box[1]*wRatio/2)
+            y2 = int(y2/2+box[1]*wRatio/2)
+        else:
+            x1 = int(x2/2-box[0]*hRatio/2)
+            x2 = int(x2/2+box[0]*hRatio/2)
+        img = img.crop((x1,y1,x2,y2))
+
+    #Resize the image with best quality algorithm ANTI-ALIAS
+    img.thumbnail(box, Image.ANTIALIAS)
+
+    #save it into a file-like object
+    #img.save(out, "JPEG", quality=90)
+    return img
+#resize
+
 @register.filter(is_safe=True)   
-def thumbnail(file, size='100x100x1'):
+def resize(file, size='100x100x1'):
     file.path = settings.MEDIA_ROOT+file.path
     print file.path+'\n'
     # defining the size
@@ -90,17 +127,20 @@ def thumbnail(file, size='100x100x1'):
         os.unlink(miniature_filename)
     # if the image wasn't already resized, resize it
     if not os.path.exists(miniature_filename):
-        image = Image.open(filename)
-        if ratio == 0:
-            image = image.resize([x, y], Image.ANTIALIAS)
-        else:
-            image.thumbnail([x, y], Image.ANTIALIAS)        
+        image = resizeandcrop(Image.open(filename), (x,y), True)
+        
+#        if ratio == 0:
+#            image = image.resize([x, y], Image.ANTIALIAS)
+#        else:
+#            image.thumbnail([x, y], Image.ANTIALIAS)        
         try:
             image.save(miniature_filename, image.format, quality=90, optimize=1)
         except:
             image.save(miniature_filename, image.format, quality=90)
 
     return miniature_url.replace(settings.MEDIA_ROOT,settings.MEDIA_URL)
+
+
 
 
 # evenements
