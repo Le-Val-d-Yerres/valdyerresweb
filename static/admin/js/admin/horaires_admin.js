3db9185abc657c3c9a4b65316282ed000a3f0331
rtrjl <rt@zone42.fr>
2012-10-02 17:13:10 +0200

    Jquery horaires custom admin

--- static/admin/js/admin/horaires_admin.js
+++ static/admin/js/admin/horaires_admin.js
@@ -1,44 +1,32 @@
-
 var $ = django.jQuery;
 
-
-
-function toggleStatus(arg) {
-		alert(arg);
-		day = $(arg).attr('id').split('_')[1];
-	
-			
-		if ($('#id_'+day+'_matin_fin').is(':disabled')) {
-			$('#id_'+day+'_matin_fin').attr('disabled',false);
-			$('#id_'+day+'_matin_fin').next().attr('disabled',false);
-		} else {
-			$('#id_'+day+'_matin_fin').attr('disabled',true);
-			$('#id_'+day+'_matin_fin').next().attr('disabled',true);
-		}
-		
-		if ( ! $('#id_'+day+'_matin_fin').is(':disabled')) {
-			$('#id_'+day+'_am_debut').attr('disabled',false);
-			$('#id_'+day+'_am_debut').next().attr('disabled',false);
-		} else {
-			$('#id_'+day+'_am_debut').attr('disabled',true);
-			$('#id_'+day+'_am_debut').next().attr('disabled',true);
-		}
-			
-}
-
-
 $(document).ready(function() {
-	var days = ['lundi','mardi']
+	var days = ['lundi', 'mardi']
 
-	
-	for (var i=0; i < days.length; i++) {
-		
-		$('#id_'+days[i]+'_journee_continue').bind('click',toggleStatus($(this)));
-	  
+	for (var i = 0; i < days.length; i++) {
+
+		$('#id_' + days[i] + '_journee_continue').bind('click', function() {
+
+			day = $(this).attr('id').split('_')[1];
+
+			if ($('#id_' + day + '_matin_fin').is(':disabled')) {
+				$('#id_' + day + '_matin_fin').attr('disabled', false);
+				$('#id_' + day + '_matin_fin').next().attr('disabled', false);
+			} else {
+				$('#id_' + day + '_matin_fin').attr('disabled', true);
+				$('#id_' + day + '_matin_fin').next().attr('disabled', true);
+			}
+
+			if (! $('#id_' + day + '_matin_fin').is(':disabled')) {
+				$('#id_' + day + '_am_debut').attr('disabled', false);
+				$('#id_' + day + '_am_debut').next().attr('disabled', false);
+			} else {
+				$('#id_' + day + '_am_debut').attr('disabled', true);
+				$('#id_' + day + '_am_debut').next().attr('disabled', true);
+			}
+
+		});
+
 	};
-	
 
-
-
-	
-});
+}); 
