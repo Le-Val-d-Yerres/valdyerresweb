f3a4314afebfb61ec755f099a7e681361c1aa0af
rtrjl <rt@zone42.fr>
2012-12-17 17:32:24 +0100

    fin dev cinemas

--- cinemas/urls.py
+++ cinemas/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('cinemas.views',
     url(r'^$', 'Seances', name="seances"),
-  
+    url(r'^(?P<cinema_slug>[^\/]+).html$', 'CinemaLieu', name="cinema"),
+    url(r'^(?P<cinema_slug>[^\/]+).vcf$', 'CinemaVCF', name="cinemavcf"),
 )
     
--- cinemas/views.py
+++ cinemas/views.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
-from django.http import Http404
+from django.http import Http404, HttpResponse
 from django.shortcuts import render_to_response , redirect , get_object_or_404
 from cinemas.models import Cinema,Film,Seance
+from django.template import Context, loader
 from pytz import timezone
 from datetime import datetime
 from valdyerresweb import settings
+from valdyerresweb.utils.functions import GenerationQrCode
+
 
 utcTZ = timezone("UTC")
 myTimezone = timezone(settings.TIME_ZONE)
@@ -14,3 +17,24 @@ def Seances(request):
     now = datetime.now(myTimezone)
     seances = Seance.objects.select_related().filter(date_debut__gt = now).order_by('film__titre','cinema__nom','date_debut')
     return render_to_response('cinemas/seances.html', {'seances' : seances})
+
+
+
+def CinemaLieu(request,cinema_slug):
+    now = datetime.now(myTimezone)
+    cinema = get_object_or_404(Cinema, slug=cinema_slug)
+    seances = Seance.objects.select_related().filter(cinema__slug = cinema_slug ,date_debut__gt = now).order_by('film__titre','cinema__nom','date_debut')
+    qr_code_vcard = GenerationQrCode(CinemaVcard(cinema))
+    qr_code_geo = GenerationQrCode("geo:" + str(cinema.latitude) + "," + str(cinema.longitude))
+    return render_to_response('cinemas/cinema-details.html', {'cinema':cinema ,'seances' : seances, 'qr_code_vcard': qr_code_vcard, 'qr_code_geo':qr_code_geo})
+
+def CinemaVCF(request,cinema_slug):
+    cinema = get_object_or_404(Cinema, slug=cinema_slug)
+    myVcard = CinemaVcard(cinema)
+    return HttpResponse(myVcard, content_type="text/vcard")
+
+
+def CinemaVcard(cinema):
+    myTemplate = loader.get_template('cinemas/cinema.vcf.html')
+    myContext = Context({"cinema": cinema, "settings": settings})
+    return myTemplate.render(myContext)
