76b560e9c2130ee2c0e320434747af2a077988e2
rtrjl <rt@zone42.fr>
2015-04-21 16:58:08 +0200

    correction horaires

--- horaires/static/admin/js/horaires_admin.js
+++ horaires/static/admin/js/horaires_admin.js
@@ -1,154 +1,149 @@
 var $ = django.jQuery;
 
 function toggleJourneeContinue(day) {
-	if ($('#id_' + day + '_journee_continue').prop('checked')) {
-		if($('#id_' + day + '_matin_ferme').prop('checked'))
-		{
-			$('#id_' + day + '_matin_ferme').attr('checked',false);
-			$('#id_' + day + '_matin_fin').attr('disabled', false);
-			$('#id_' + day + '_matin_fin').next().attr('disabled', false);
-			$('#id_' + day + '_matin_debut').attr('disabled', false);
-			$('#id_' + day + '_matin_debut').next().attr('disabled', false);
-			$('#id_' + day + '_matin_fin').next().css('background-position', ' 0 -1908px');
-			$('#id_' + day + '_matin_debut').next().css('background-position', ' 0 -1908px');
-            $('#id_' + day + '_matin_fin').next().css('background-color', '#EEE');
-			$('#id_' + day + '_matin_debut').next().css('background-color', '#EEE');
-		}
-		
-		if($('#id_' + day + '_am_ferme').prop('checked'))
-		{
-			$('#id_' + day + '_am_ferme').attr('checked',false);
-			$('#id_' + day + '_am_fin').attr('disabled', false);
-			$('#id_' + day + '_am_fin').next().attr('disabled', false);
-			$('#id_' + day + '_am_debut').attr('disabled', false);
-			$('#id_' + day + '_am_debut').next().attr('disabled', false);
-			$('#id_' + day + '_am_fin').next().css('background-position', ' 0 -1908px');
-			$('#id_' + day + '_am_debut').next().css('background-position', ' 0 -1908px');
-            $('#id_' + day + '_am_fin').next().css('background-color', '#EEE');
-			$('#id_' + day + '_am_debut').next().css('background-position', '#EEE');
-			
-		}
-		$('#id_' + day + '_matin_fin').attr('disabled', true);
-		$('#id_' + day + '_matin_fin').next().attr('disabled', true);
-		$('#id_' + day + '_am_debut').attr('disabled', true);
-		$('#id_' + day + '_am_debut').next().attr('disabled', true);
-		$('#id_' + day + '_matin_fin').next().css('background-position', ' 0 -1908px');
-		$('#id_' + day + '_am_debut').next().css('background-position', ' 0 -1908px');
-        $('#id_' + day + '_matin_fin').next().css('background-color', '#EEE');
-		$('#id_' + day + '_am_debut').next().css('background-color', '#EEE');
-		$('#id_' + day + '_matin_fin').val('');
-		$('#id_' + day + '_am_debut').val('');
-		$('#id_' + day + '_matin_ferme').attr('disabled',true);
-		$('#id_' + day + '_am_ferme').attr('disabled',true);
-		
-	} else {
-		$('#id_' + day + '_matin_fin').attr('disabled', false);
-		$('#id_' + day + '_matin_fin').next().attr('disabled', false);
-		$('#id_' + day + '_am_debut').attr('disabled', false);
-		$('#id_' + day + '_am_debut').next().attr('disabled', false);
-		$('#id_' + day + '_matin_fin').next().css('background-position', ' 0 -1865px');
-		$('#id_' + day + '_am_debut').next().css('background-position', ' 0 -1865px');
-        $('#id_' + day + '_matin_fin').next().css('background-color', '#EEE');
-		$('#id_' + day + '_am_debut').next().css('background-color', '#EEE');
-		$('#id_' + day + '_matin_ferme').attr('disabled',false);
-		$('#id_' + day + '_am_ferme').attr('disabled',false);
+    if ($('#id_' + day + '_journee_continue').prop('checked')) {
+        if ($('#id_' + day + '_matin_ferme').prop('checked')) {
+            toggleFermetureMatin(day);
+        }
 
-	}
+        if ($('#id_' + day + '_am_ferme').prop('checked')) {
+            toggleFermetureAM(day);
+        }
+        $('#id_' + day + '_matin_fin').attr('disabled', true);
+        $('#id_' + day + '_matin_fin').next().attr('disabled', true);
+        $('#id_' + day + '_am_debut').attr('disabled', true);
+        $('#id_' + day + '_am_debut').next().attr('disabled', true);
+        $('#id_' + day + '_matin_fin').next().css('background-position', ' 0 -1908px');
+        $('#id_' + day + '_am_debut').next().css('background-position', ' 0 -1908px');
+        $('#id_' + day + '_matin_fin').next().css('background-color', '#EEE');
+        $('#id_' + day + '_am_debut').next().css('background-color', '#EEE');
+        $('#id_' + day + '_matin_fin').val('');
+        $('#id_' + day + '_am_debut').val('');
+        $('#id_' + day + '_matin_ferme').attr('disabled', true);
+        $('#id_' + day + '_am_ferme').attr('disabled', true);
+
+    } else {
+        if ($('#id_' + day + '_matin_ferme').prop('checked')) {
+            toggleFermetureMatin(day);
+        }
+        else {
+            $('#id_' + day + '_matin_fin').attr('disabled', false);
+            $('#id_' + day + '_matin_fin').next().attr('disabled', false);
+            $('#id_' + day + '_matin_fin').next().css('background-position', ' 0 -1865px');
+            $('#id_' + day + '_matin_fin').next().css('background-color', '#EEE');
+            $('#id_' + day + '_matin_ferme').attr('disabled', false);
+        }
+
+        if ($('#id_' + day + '_am_ferme').prop('checked')) {
+            toggleFermetureAM(day);
+        }
+        else {
+            $('#id_' + day + '_am_debut').attr('disabled', false);
+            $('#id_' + day + '_am_debut').next().attr('disabled', false);
+            $('#id_' + day + '_am_debut').next().css('background-position', ' 0 -1865px');
+            $('#id_' + day + '_am_debut').next().css('background-color', '#EEE');
+            $('#id_' + day + '_am_ferme').attr('disabled', false);
+        }
+
+
+    }
 
 }
 
 function toggleFermetureMatin(day) {
-	if ($('#id_' + day + '_matin_ferme').prop('checked')) {
-		$('#id_' + day + '_matin_fin').attr('disabled', true);
-		$('#id_' + day + '_matin_fin').next().attr('disabled', true);
-		$('#id_' + day + '_matin_debut').attr('disabled', true);
-		$('#id_' + day + '_matin_debut').next().attr('disabled', true);
-		$('#id_' + day + '_matin_debut').next().attr('disabled', true);
-		$('#id_' + day + '_matin_fin').next().css('background-position', ' 0 -1908px');
-		$('#id_' + day + '_matin_debut').next().css('background-position', ' 0 -1908px');
+    if ($('#id_' + day + '_matin_ferme').prop('checked')) {
+        $('#id_' + day + '_matin_fin').attr('disabled', true);
+        $('#id_' + day + '_matin_fin').next().attr('disabled', true);
+        $('#id_' + day + '_matin_debut').attr('disabled', true);
+        $('#id_' + day + '_matin_debut').next().attr('disabled', true);
+        $('#id_' + day + '_matin_debut').next().attr('disabled', true);
+        $('#id_' + day + '_matin_fin').next().css('background-position', ' 0 -1908px');
+        $('#id_' + day + '_matin_debut').next().css('background-position', ' 0 -1908px');
         $('#id_' + day + '_matin_fin').next().css('background-color', '#EEE');
-		$('#id_' + day + '_matin_debut').next().css('background-color', '#EEE');
-		$('#id_' + day + '_matin_fin').val('');
-		$('#id_' + day + '_matin_debut').val('');
-		$('#id_' + day + '_journee_continue').attr('disabled', true)
-	} else {
-		$('#id_' + day + '_matin_fin').attr('disabled', false);
-		$('#id_' + day + '_matin_fin').next().attr('disabled', false);
-		$('#id_' + day + '_matin_debut').attr('disabled', false);
-		$('#id_' + day + '_matin_debut').next().attr('disabled', false);
-		$('#id_' + day + '_matin_fin').next().css('background-position', ' 0 -1865px');
-		$('#id_' + day + '_matin_debut').next().css('background-position', ' 0 -1865px');
+        $('#id_' + day + '_matin_debut').next().css('background-color', '#EEE');
+        $('#id_' + day + '_matin_fin').val('');
+        $('#id_' + day + '_matin_debut').val('');
+        $('#id_' + day + '_journee_continue').attr('disabled', true)
+    } else {
+        $('#id_' + day + '_matin_fin').attr('disabled', false);
+        $('#id_' + day + '_matin_fin').next().attr('disabled', false);
+        $('#id_' + day + '_matin_debut').attr('disabled', false);
+        $('#id_' + day + '_matin_debut').next().attr('disabled', false);
+        $('#id_' + day + '_matin_fin').next().css('background-position', ' 0 -1865px');
+        $('#id_' + day + '_matin_debut').next().css('background-position', ' 0 -1865px');
         $('#id_' + day + '_matin_fin').next().css('background-color', '#E1F0F5');
-		$('#id_' + day + '_matin_debut').next().css('background-color', '#E1F0F5');
-		$('#id_' + day + '_journee_continue').attr('disabled', false)
+        $('#id_' + day + '_matin_debut').next().css('background-color', '#E1F0F5');
+        $('#id_' + day + '_journee_continue').attr('disabled', false)
 
-	}
+    }
 
 }
 
 
 function toggleFermetureAM(day) {
-	if ($('#id_' + day + '_am_ferme').prop('checked')) {
-		$('#id_' + day + '_am_fin').attr('disabled', true);
-		$('#id_' + day + '_am_fin').next().attr('disabled', true);
-		$('#id_' + day + '_am_debut').attr('disabled', true);
-		$('#id_' + day + '_am_debut').next().attr('disabled', true);
-		$('#id_' + day + '_am_debut').next().attr('disabled', true);
-		$('#id_' + day + '_am_fin').next().css('background-position', ' 0 -1908px');
-		$('#id_' + day + '_am_debut').next().css('background-position', ' 0 -1908px');
+    if ($('#id_' + day + '_am_ferme').prop('checked')) {
+        $('#id_' + day + '_am_fin').attr('disabled', true);
+        $('#id_' + day + '_am_fin').next().attr('disabled', true);
+        $('#id_' + day + '_am_debut').attr('disabled', true);
+        $('#id_' + day + '_am_debut').next().attr('disabled', true);
+        $('#id_' + day + '_am_debut').next().attr('disabled', true);
+        $('#id_' + day + '_am_fin').next().css('background-position', ' 0 -1908px');
+        $('#id_' + day + '_am_debut').next().css('background-position', ' 0 -1908px');
         $('#id_' + day + '_am_fin').next().css('background-color', '#EEE');
-		$('#id_' + day + '_am_debut').next().css('background-color', '#EEE');
-		$('#id_' + day + '_am_fin').val('');
-		$('#id_' + day + '_am_debut').val('');
-		$('#id_' + day + '_journee_continue').attr('disabled', true)
+        $('#id_' + day + '_am_debut').next().css('background-color', '#EEE');
+        $('#id_' + day + '_am_fin').val('');
+        $('#id_' + day + '_am_debut').val('');
+        $('#id_' + day + '_journee_continue').attr('disabled', true)
 
-	} else {
-		$('#id_' + day + '_am_fin').attr('disabled', false);
-		$('#id_' + day + '_am_fin').next().attr('disabled', false);
-		$('#id_' + day + '_am_debut').attr('disabled', false);
-		$('#id_' + day + '_am_debut').next().attr('disabled', false);
-		$('#id_' + day + '_am_fin').next().css('background-position', ' 0 -1865px');
-		$('#id_' + day + '_am_debut').next().css('background-position', ' 0 -1865px');
+    } else {
+        $('#id_' + day + '_am_fin').attr('disabled', false);
+        $('#id_' + day + '_am_fin').next().attr('disabled', false);
+        $('#id_' + day + '_am_debut').attr('disabled', false);
+        $('#id_' + day + '_am_debut').next().attr('disabled', false);
+        $('#id_' + day + '_am_fin').next().css('background-position', ' 0 -1865px');
+        $('#id_' + day + '_am_debut').next().css('background-position', ' 0 -1865px');
         $('#id_' + day + '_am_fin').next().css('background-color', '#E1F0F5');
-		$('#id_' + day + '_am_debut').next().css('background-color', '#E1F0F5');
-		$('#id_' + day + '_journee_continue').attr('disabled', false)
-	}
+        $('#id_' + day + '_am_debut').next().css('background-color', '#E1F0F5');
+        $('#id_' + day + '_journee_continue').attr('disabled', false)
+    }
 
 }
 
-$(document).ready(function() {
-	var days = ['lundi', 'mardi','mercredi', 'jeudi', 'vendredi', 'samedi', 'dimanche']
+$(document).ready(function () {
+    var days = ['lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi', 'dimanche']
     var i = 0;
-	for (i = 0; i < days.length; i++) {
+    for (i = 0; i < days.length; i++) {
 
-		$('#id_' + days[i] + '_journee_continue').bind('click', function() {
+        $('#id_' + days[i] + '_journee_continue').bind('click', function () {
 
-			day = $(this).attr('id').split('_')[1];
-			
-			toggleJourneeContinue(day);
-			
+            day = $(this).attr('id').split('_')[1];
 
-		});
+            toggleJourneeContinue(day);
 
-		$('#id_' + days[i] + '_matin_ferme').bind('click', function() {
 
-			day = $(this).attr('id').split('_')[1];
+        });
 
-			toggleFermetureMatin(day);
+        $('#id_' + days[i] + '_matin_ferme').bind('click', function () {
 
-		});
-		$('#id_' + days[i] + '_am_ferme').bind('click', function() {
+            day = $(this).attr('id').split('_')[1];
 
-			day = $(this).attr('id').split('_')[1];
+            toggleFermetureMatin(day);
 
-			toggleFermetureAM(day);
+        });
+        $('#id_' + days[i] + '_am_ferme').bind('click', function () {
 
-		});
+            day = $(this).attr('id').split('_')[1];
 
-        toggleJourneeContinue(days[i]);
+            toggleFermetureAM(day);
+
+        });
+
+
         toggleFermetureMatin(days[i]);
         toggleFermetureAM(days[i]);
-		
-	};
+        toggleJourneeContinue(days[i]);
+
+    }
+    ;
 
 });
