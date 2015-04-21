53efdc4cbcf0725ccf482915fa6ff3f91468cff1
rtrjl <rt@zone42.fr>
2015-04-21 16:44:01 +0200

    correction horaires

--- horaires/static/admin/js/horaires_admin.js
+++ horaires/static/admin/js/horaires_admin.js
@@ -1,35 +1,41 @@
 var $ = django.jQuery;
 
 function toggleJourneeContinue(day) {
-	if ($('#id_' + day + '_journee_continue').attr('checked')) {
-		if($('#id_' + day + '_matin_ferme').attr('checked'))
+	if ($('#id_' + day + '_journee_continue').prop('checked')) {
+		if($('#id_' + day + '_matin_ferme').prop('checked'))
 		{
 			$('#id_' + day + '_matin_ferme').attr('checked',false);
 			$('#id_' + day + '_matin_fin').attr('disabled', false);
 			$('#id_' + day + '_matin_fin').next().attr('disabled', false);
 			$('#id_' + day + '_matin_debut').attr('disabled', false);
 			$('#id_' + day + '_matin_debut').next().attr('disabled', false);
-			$('#id_' + day + '_matin_fin').next().css('background-position', ' 0 -1691px');
-			$('#id_' + day + '_matin_debut').next().css('background-position', ' 0 -1691px');
+			$('#id_' + day + '_matin_fin').next().css('background-position', ' 0 -1908px');
+			$('#id_' + day + '_matin_debut').next().css('background-position', ' 0 -1908px');
+            $('#id_' + day + '_matin_fin').next().css('background-color', '#EEE');
+			$('#id_' + day + '_matin_debut').next().css('background-color', '#EEE');
 		}
 		
-		if($('#id_' + day + '_am_ferme').attr('checked'))
+		if($('#id_' + day + '_am_ferme').prop('checked'))
 		{
 			$('#id_' + day + '_am_ferme').attr('checked',false);
 			$('#id_' + day + '_am_fin').attr('disabled', false);
 			$('#id_' + day + '_am_fin').next().attr('disabled', false);
 			$('#id_' + day + '_am_debut').attr('disabled', false);
 			$('#id_' + day + '_am_debut').next().attr('disabled', false);
-			$('#id_' + day + '_am_fin').next().css('background-position', ' 0 -1691px');
-			$('#id_' + day + '_am_debut').next().css('background-position', ' 0 -1691px');
+			$('#id_' + day + '_am_fin').next().css('background-position', ' 0 -1908px');
+			$('#id_' + day + '_am_debut').next().css('background-position', ' 0 -1908px');
+            $('#id_' + day + '_am_fin').next().css('background-color', '#EEE');
+			$('#id_' + day + '_am_debut').next().css('background-position', '#EEE');
 			
 		}
 		$('#id_' + day + '_matin_fin').attr('disabled', true);
 		$('#id_' + day + '_matin_fin').next().attr('disabled', true);
 		$('#id_' + day + '_am_debut').attr('disabled', true);
 		$('#id_' + day + '_am_debut').next().attr('disabled', true);
-		$('#id_' + day + '_matin_fin').next().css('background-position', ' 0 -1734px');
-		$('#id_' + day + '_am_debut').next().css('background-position', ' 0 -1734px');
+		$('#id_' + day + '_matin_fin').next().css('background-position', ' 0 -1908px');
+		$('#id_' + day + '_am_debut').next().css('background-position', ' 0 -1908px');
+        $('#id_' + day + '_matin_fin').next().css('background-color', '#EEE');
+		$('#id_' + day + '_am_debut').next().css('background-color', '#EEE');
 		$('#id_' + day + '_matin_fin').val('');
 		$('#id_' + day + '_am_debut').val('');
 		$('#id_' + day + '_matin_ferme').attr('disabled',true);
@@ -40,8 +46,10 @@ function toggleJourneeContinue(day) {
 		$('#id_' + day + '_matin_fin').next().attr('disabled', false);
 		$('#id_' + day + '_am_debut').attr('disabled', false);
 		$('#id_' + day + '_am_debut').next().attr('disabled', false);
-		$('#id_' + day + '_matin_fin').next().css('background-position', ' 0 -1691px');
-		$('#id_' + day + '_am_debut').next().css('background-position', ' 0 -1691px');
+		$('#id_' + day + '_matin_fin').next().css('background-position', ' 0 -1865px');
+		$('#id_' + day + '_am_debut').next().css('background-position', ' 0 -1865px');
+        $('#id_' + day + '_matin_fin').next().css('background-color', '#EEE');
+		$('#id_' + day + '_am_debut').next().css('background-color', '#EEE');
 		$('#id_' + day + '_matin_ferme').attr('disabled',false);
 		$('#id_' + day + '_am_ferme').attr('disabled',false);
 
@@ -50,14 +58,16 @@ function toggleJourneeContinue(day) {
 }
 
 function toggleFermetureMatin(day) {
-	if ($('#id_' + day + '_matin_ferme').attr('checked')) {
+	if ($('#id_' + day + '_matin_ferme').prop('checked')) {
 		$('#id_' + day + '_matin_fin').attr('disabled', true);
 		$('#id_' + day + '_matin_fin').next().attr('disabled', true);
 		$('#id_' + day + '_matin_debut').attr('disabled', true);
 		$('#id_' + day + '_matin_debut').next().attr('disabled', true);
 		$('#id_' + day + '_matin_debut').next().attr('disabled', true);
-		$('#id_' + day + '_matin_fin').next().css('background-position', ' 0 -1734px');
-		$('#id_' + day + '_matin_debut').next().css('background-position', ' 0 -1734px');
+		$('#id_' + day + '_matin_fin').next().css('background-position', ' 0 -1908px');
+		$('#id_' + day + '_matin_debut').next().css('background-position', ' 0 -1908px');
+        $('#id_' + day + '_matin_fin').next().css('background-color', '#EEE');
+		$('#id_' + day + '_matin_debut').next().css('background-color', '#EEE');
 		$('#id_' + day + '_matin_fin').val('');
 		$('#id_' + day + '_matin_debut').val('');
 		$('#id_' + day + '_journee_continue').attr('disabled', true)
@@ -66,8 +76,10 @@ function toggleFermetureMatin(day) {
 		$('#id_' + day + '_matin_fin').next().attr('disabled', false);
 		$('#id_' + day + '_matin_debut').attr('disabled', false);
 		$('#id_' + day + '_matin_debut').next().attr('disabled', false);
-		$('#id_' + day + '_matin_fin').next().css('background-position', ' 0 -1691px');
-		$('#id_' + day + '_matin_debut').next().css('background-position', ' 0 -1691px');
+		$('#id_' + day + '_matin_fin').next().css('background-position', ' 0 -1865px');
+		$('#id_' + day + '_matin_debut').next().css('background-position', ' 0 -1865px');
+        $('#id_' + day + '_matin_fin').next().css('background-color', '#E1F0F5');
+		$('#id_' + day + '_matin_debut').next().css('background-color', '#E1F0F5');
 		$('#id_' + day + '_journee_continue').attr('disabled', false)
 
 	}
@@ -76,14 +88,16 @@ function toggleFermetureMatin(day) {
 
 
 function toggleFermetureAM(day) {
-	if ($('#id_' + day + '_am_ferme').attr('checked')) {
+	if ($('#id_' + day + '_am_ferme').prop('checked')) {
 		$('#id_' + day + '_am_fin').attr('disabled', true);
 		$('#id_' + day + '_am_fin').next().attr('disabled', true);
 		$('#id_' + day + '_am_debut').attr('disabled', true);
 		$('#id_' + day + '_am_debut').next().attr('disabled', true);
 		$('#id_' + day + '_am_debut').next().attr('disabled', true);
-		$('#id_' + day + '_am_fin').next().css('background-position', ' 0 -1934px');
-		$('#id_' + day + '_am_debut').next().css('background-position', ' 0 -1934px');
+		$('#id_' + day + '_am_fin').next().css('background-position', ' 0 -1908px');
+		$('#id_' + day + '_am_debut').next().css('background-position', ' 0 -1908px');
+        $('#id_' + day + '_am_fin').next().css('background-color', '#EEE');
+		$('#id_' + day + '_am_debut').next().css('background-color', '#EEE');
 		$('#id_' + day + '_am_fin').val('');
 		$('#id_' + day + '_am_debut').val('');
 		$('#id_' + day + '_journee_continue').attr('disabled', true)
@@ -93,8 +107,10 @@ function toggleFermetureAM(day) {
 		$('#id_' + day + '_am_fin').next().attr('disabled', false);
 		$('#id_' + day + '_am_debut').attr('disabled', false);
 		$('#id_' + day + '_am_debut').next().attr('disabled', false);
-		$('#id_' + day + '_am_fin').next().css('background-position', ' 0 -1691px');
-		$('#id_' + day + '_am_debut').next().css('background-position', ' 0 -1691px');
+		$('#id_' + day + '_am_fin').next().css('background-position', ' 0 -1865px');
+		$('#id_' + day + '_am_debut').next().css('background-position', ' 0 -1865px');
+        $('#id_' + day + '_am_fin').next().css('background-color', '#E1F0F5');
+		$('#id_' + day + '_am_debut').next().css('background-color', '#E1F0F5');
 		$('#id_' + day + '_journee_continue').attr('disabled', false)
 	}
 
@@ -102,8 +118,8 @@ function toggleFermetureAM(day) {
 
 $(document).ready(function() {
 	var days = ['lundi', 'mardi','mercredi', 'jeudi', 'vendredi', 'samedi', 'dimanche']
-
-	for (var i = 0; i < days.length; i++) {
+    var i = 0;
+	for (i = 0; i < days.length; i++) {
 
 		$('#id_' + days[i] + '_journee_continue').bind('click', function() {
 
@@ -128,6 +144,10 @@ $(document).ready(function() {
 			toggleFermetureAM(day);
 
 		});
+
+        toggleJourneeContinue(days[i]);
+        toggleFermetureMatin(days[i]);
+        toggleFermetureAM(days[i]);
 		
 	};
 
