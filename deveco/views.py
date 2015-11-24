725e98a877e8a20ed2cd741f96f66b90079e4d55
rtrjl <rt@zone42.fr>
2015-11-24 16:41:47 +0100

    suite deveco

--- deveco/views.py
+++ deveco/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response , redirect , get_object_or_404
 
-# Create your views here.
+
+def Home(request):
+    params = {}
+    render_to_response("deveco/")
