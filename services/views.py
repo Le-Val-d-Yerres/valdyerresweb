083e6bb5f49fe71754b068be330d112b74ad5a73
rtrjl <rt@zone42.fr>
2016-07-05 16:20:15 +0200

    suite dev evo

--- services/views.py
+++ services/views.py
@@ -17,11 +17,10 @@ def ServiceDetail(request,service_slug):
     service = get_object_or_404(Service.objects.select_related(),slug = service_slug)
     pages_liees = PageStatiqueService.objects.filter(service = service.id,publie = True).select_related().order_by('index')
     equipements = Equipement.objects.select_related().filter(fonction__service= service.id)
-    nb_annonces = Annonce.objects.filter(service=service.id).count()
+    nb_annonces = Annonce.objects.filter(service=service.id, publie=True).count()
     return render_to_response('services/service-detail.html', {'service' : service, 'pages_liees': pages_liees, 'nb_annonces':nb_annonces ,'equipements': equipements})
 
 def PageContenu(request, service_slug ,page_slug):
-    
-    page = get_object_or_404(PageStatiqueService.objects.select_related(), slug = page_slug, service__slug = service_slug, publie = True )
+    page = get_object_or_404(PageStatiqueService.objects.select_related(), slug = page_slug, service__slug = service_slug, publie=True)
     documentattache = DocumentAttache.objects.filter(reference_id = page.id)
     return render_to_response('services/page-service.html', {'page' : page, 'documentattache':documentattache})
