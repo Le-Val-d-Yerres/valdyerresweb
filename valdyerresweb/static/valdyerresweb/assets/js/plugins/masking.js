f5a6b83c4bf7cd4e7593dfc395bca14005240fac
rtrjl <rt@zone42.fr>
2017-12-22 14:59:10 +0100

    modifs epn

--- valdyerresweb/static/valdyerresweb/assets/js/plugins/masking.js
+++ valdyerresweb/static/valdyerresweb/assets/js/plugins/masking.js
@@ -4,8 +4,9 @@ var Masking = function () {
         
         //Masking
         initMasking: function () {
-	        $("#date1").mask('99/99/9999', {placeholder:'X'});
-	        $("#phone").mask('(999) 999-9999', {placeholder:'X'});
+	        $(".date1").mask('99/99/9999', {placeholder:'_'});
+	        $("#phone").mask('99 99 99 99 99', {placeholder:'_'});
+	        $("#postal").mask('99999', {placeholder:'_'});
 	        $("#card").mask('9999-9999-9999-9999', {placeholder:'X'});
 	        $("#serial").mask('***-***-***-***-***-***', {placeholder:'_'});
 	        $("#tax").mask('99-9999999', {placeholder:'X'});
